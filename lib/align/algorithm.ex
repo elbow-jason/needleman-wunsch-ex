50e06a66c313ae2659ac729aa59f8b6c64b32f5e
jlgoldb2 <jlgoldb2@asu.edu>
2015-02-01 00:28:01 -0700

    initial matrix generation is working

--- lib/align/algorithm.ex
+++ lib/align/algorithm.ex
@@ -10,4 +10,15 @@ defmodule Align.Algorithm do
     list1 = Enum.map(1..seq1len, fn x -> 0 end)
     Enum.map(1..seq2len, fn x -> list1 end)
   end
+
+  def gapify([first_row | two_d_list], gap_penalty) do
+    first_row = Enum.map(first_row, fn x -> gap_penalty end)
+    gapify_first_col = fn x -> first_is_gap(x, gap_penalty) end
+    gap_in_first_col = Enum.map(two_d_list, gapify_first_col)
+    [first_row | gap_in_first_col]
+  end
+
+  def first_is_gap([_head | rest], gap_penalty) do
+    [gap_penalty | rest]
+  end
 end
--- test/align_test.exs
+++ test/align_test.exs
@@ -1,8 +1,23 @@
 defmodule AlignTest do
   use ExUnit.Case
-
+  import Align.Algorithm
 
   test "two_dimensional_list gives a 2-D list" do
-    assert Align.Algorithm.zeros(2,2) == [[0,0], [0,0]]
+    assert zeros(2,2) == [[0,0],
+                                          [0,0]]
+
+    assert zeros(3,2) == [[0,0,0],
+                                          [0,0,0]]
   end
+
+  test "first_is_gap makes the 0th element the gap_penalty" do
+    assert first_is_gap([0,0,0,0,0], 10) == [10,0,0,0,0]
+  end
+
+  test "gapify makes all first row and column values the gap_penalty" do
+    my_list = [[0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0]]
+    expected  = [[10,10,10,10,10], [10,0,0,0,0], [10,0,0,0,0]]
+    assert gapify(my_list, 10) == expected
+  end
+
 end
